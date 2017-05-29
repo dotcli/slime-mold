53b7ecccf51997b8cb3eba158e756c1c5fdf846d
lcb931023 <lcb931023@gmail.com>
2017-05-29 16:43:04 -0700

    mouse raycasting succeeded

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 slime-mold
 ========
-slime mold behavior simulation. 
+Slime mold behavior simulation. Implemented via flocking and pheromones seeking?
+
+## research
+
+This might be harder than it appears... however, the look and basic behavior should be easier to simulate than the actual organism.
+
+https://en.wikipedia.org/wiki/Physarum_polycephalum 
+> As the slime mould does not have any nervous system that could explain these intelligent behaviours, there has been considerable interdisciplinary interest in understanding the rules that govern its behaviour. Scientists are trying to model the slime mold using a number of simple, distributed rules. For example, P. polycephalum has been modeled as a set of differential equations inspired by electrical networks. This model can be shown to be able to compute shortest paths.[12] A very similar model can be shown to solve the Steiner tree problem.[7] However, currently these models do not make sense biologically, as they for example assume energy conservation inside the slime mould. Living organisms consume food, so energy can not be conserved. To build more realistic models, more data about the slime mould's network construction needs to be gathered. To this end, researchers are analysing the network structure of lab-grown P. polycephalum.[13]
+
+http://www.pnas.org/content/109/43/17490.long
+
+A pheromone + flocking based approach was done before:
+https://books.google.com/books?id=0gT1zMLEDloC&pg=PA427&lpg=PA427&dq=slime+mold+pheromone&source=bl&ots=dCrokp5bSi&sig=soBc3P8sRTmehZUMV9xWKG6Fq24&hl=en&sa=X&ved=0ahUKEwiJ18-L5JTUAhVU02MKHQ_rBtgQ6AEIQzAE#v=onepage&q=slime%20mold%20pheromone&f=false
+
+We could make it into something useful and pretty
 
 ## License
  
--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-const randomColor = require('randomcolor');
+const randomColor = require( 'randomcolor' );
 
-global.THREE = require('three');
-const TWEEN = require('tween.js');
-const createOrbitViewer = require('three-orbit-viewer')(THREE)
+global.THREE = require( 'three' );
+const createOrbitViewer = require( 'three-orbit-viewer' )( THREE );
+
+const SlimeMold = require('./lib/SlimeMold')
 
 /* Scene */
-var app = createOrbitViewer({
+const app = createOrbitViewer( {
   clearColor: 0x000000,
   clearAlpha: 1,
-  fov: 65,
-  position: new THREE.Vector3(0, 0, 1), // camera
-})
+  fov: 50,
+  position: new THREE.Vector3( 0, 0, 1 ), // camera
+} );
 
-app.on('tick', function(dt) {
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry( 1, 1, 1, 1 ),
+  new THREE.MeshBasicMaterial()
+);
+const wireframe = new THREE.LineSegments(
+  new THREE.WireframeGeometry(
+    new THREE.PlaneGeometry( 1, 1, 10, 10 )
+  ),
+  new THREE.LineBasicMaterial({ color: 0x000000 })
+);
+
+app.scene.add( plane );
+app.scene.add( wireframe );
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+app.on( 'tick', function ( dt ) {
   //.. handle pre-render updates
-  TWEEN.update();
-})
+  raycaster.setFromCamera( mouse, app.camera );
+} );
 
+window.addEventListener( 'click', germinate );
+window.addEventListener( 'mousemove', updateMouse );
+
+function germinate() {
+  let intersects = raycaster.intersectObjects( app.scene.children );
+  if (intersects.length === 0) return;
+  for (var i = 0; i < intersects.length; i ++) {
+    if (intersects[i].object.id === plane.id) {
+      // NOTE so yeah this is how you add the thing
+      // TODO spawn slimeMold
+      let box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial()
+      )
+      box.scale.multiplyScalar( 0.01 )
+      box.position.copy( intersects[i].point )
+      app.scene.add(box)
+    }
+  }
+}
+
+function updateMouse( e ) {
+  mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+}
