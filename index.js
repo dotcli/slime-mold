de3fa1f9ba1f57415936e0cca4ad16a5d75612a2
lcb931023 <lcb931023@gmail.com>
2017-05-29 20:35:26 -0700

    cool ranch flavored growth with intentional z-axis bug

--- index.js
+++ index.js
@@ -1,61 +1,82 @@
-const randomColor = require( 'randomcolor' );
-
 global.THREE = require( 'three' );
+const TWEEN = require('tween.js');
 const createOrbitViewer = require( 'three-orbit-viewer' )( THREE );
 
-const SlimeMold = require('./lib/SlimeMold')
+import SlimeMold from './lib/SlimeMold'
 
 /* Scene */
 const app = createOrbitViewer( {
   clearColor: 0x000000,
   clearAlpha: 1,
-  fov: 50,
-  position: new THREE.Vector3( 0, 0, 1 ), // camera
+  fov: 1,
+  position: new THREE.Vector3( 0, 0, 20 ), // camera
 } );
 
 const plane = new THREE.Mesh(
   new THREE.PlaneGeometry( 1, 1, 1, 1 ),
-  new THREE.MeshBasicMaterial()
+  new THREE.MeshBasicMaterial({ color: 0x000000 })
 );
-const wireframe = new THREE.LineSegments(
-  new THREE.WireframeGeometry(
-    new THREE.PlaneGeometry( 1, 1, 10, 10 )
-  ),
-  new THREE.LineBasicMaterial({ color: 0x000000 })
-);
-
+plane.material.transparent = true
+plane.material.opacity = 0
 app.scene.add( plane );
-app.scene.add( wireframe );
+
+// const wireframe = new THREE.LineSegments(
+//   new THREE.WireframeGeometry(
+//     new THREE.PlaneGeometry( 1, 1, 10, 10 )
+//   ),
+//   new THREE.LineBasicMaterial({ color: 0x000000 })
+// );
+// app.scene.add( wireframe );
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-app.on( 'tick', function ( dt ) {
-  //.. handle pre-render updates
-  raycaster.setFromCamera( mouse, app.camera );
-} );
 
-window.addEventListener( 'click', germinate );
+const molds = []
+
+window.addEventListener( 'click', germinateOnRaycast );
 window.addEventListener( 'mousemove', updateMouse );
 
-function germinate() {
-  let intersects = raycaster.intersectObjects( app.scene.children );
-  if (intersects.length === 0) return;
-  for (var i = 0; i < intersects.length; i ++) {
-    if (intersects[i].object.id === plane.id) {
-      // NOTE so yeah this is how you add the thing
-      // TODO spawn slimeMold
-      let box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial()
-      )
-      box.scale.multiplyScalar( 0.01 )
-      box.position.copy( intersects[i].point )
-      app.scene.add(box)
-    }
+window.addEventListener( 'keypress', (e)=>{
+  if (e.key === ' ') {
+    const start = app.camera.position.z > 0 ? 0 : Math.PI
+    const end = start + Math.PI;
+    let tween = new TWEEN.Tween({ angle: start })
+      .to({ angle: end }, 4000)
+      .easing(TWEEN.Easing.Quartic.InOut)
+      .onUpdate(function () {
+        console.log(this.angle);
+        app.camera.position.set(Math.sin(this.angle) * 20, 0, Math.cos(this.angle) * 20)
+        app.camera.lookAt(new THREE.Vector3())
+      })
+    tween.start()
   }
+} )
+
+function germinateOnRaycast() {
+  let intersects = raycaster.intersectObject( plane );
+  if (intersects.length === 0) return;
+  germinate( intersects[0].point )
+}
+
+function germinate(point) {
+  // NOTE so yeah this is where you add the thing
+  // TODO spawn slime mold
+  let sm = new SlimeMold()
+  sm.position.copy( point )
+  molds.push(sm)
+  app.scene.add(sm)
 }
 
 function updateMouse( e ) {
   mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
   mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
 }
+
+app.on( 'tick', function ( dt ) {
+  //.. handle pre-render updates
+  raycaster.setFromCamera( mouse, app.camera );
+  molds.forEach((sm) => {
+    sm.grow(dt)
+  })
+  TWEEN.update();
+} );
